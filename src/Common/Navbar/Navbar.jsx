ab653198cf0051e846016e5a70a2d4b46ab94a8d
Anik Islam <anik54908@gmail.com>
2026-04-19 01:12:00 +0600

    created header / navbar

--- src/Common/Navbar/Navbar.jsx
+++ src/Common/Navbar/Navbar.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import Container from '../Container/Container'
+import React from "react";
+import Container from "../Container/Container";
 
-
+import logo from "../../assets/img/logo.png";
 
 const Navbar = () => {
   return (
     <>
-    <section>
+      <section>
         <Container>
-          <div>
+          <div className="flex items-center justify-between gap-77 bg-[#ddd] p-[15px_10px_15px_10px]">
             <div>
-                <img src="" alt="" />
+              <img className="w-[80px]" src={logo} alt="logo image" />
             </div>
-            <div></div>
+            <div className="flex items-center gap-14">
+              <nav>
+                <ul className="flex items-center gap-14">
+                  <li className="text-[18px] font-semibold font-raleway leading-6.5 text-[#0D0600] hover:text-[#f16722] duration-300 ">
+                    <a href="#">Home</a>
+                  </li>
+                  <li className="text-[18px] font-semibold font-raleway leading-6.5 text-[#0D0600] hover:text-[#f16722] duration-300 ">
+                    <a href="#">About Us</a>
+                  </li>
+                  <li className="text-[18px] font-semibold font-raleway leading-6.5 text-[#0D0600] hover:text-[#f16722] duration-300 ">
+                    <a href="#">Our Service</a>
+                  </li>
+                  <li className="text-[18px] font-semibold font-raleway leading-6.5 text-[#0D0600] hover:text-[#f16722] duration-300 ">
+                    <a href="#">Our Work</a>
+                  </li>
+                  <li className="text-[18px] font-semibold font-raleway leading-6.5 text-[#0D0600] hover:text-[#f16722] duration-300 ">
+                    <a href="#">Contact Us</a>
+                  </li>
+                </ul>
+              </nav>
+              <div>
+                <button className="text-white text-[18px] font-bold leading-6.25 bg-[#f16722] py-2 px-8 rounded-[30px] cursor-pointer border border-[#f16722] hover:bg-transparent hover:text-[#f16722] duration-300 ">Get A Quote</button>
+              </div>
+            </div>
           </div>
         </Container>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
