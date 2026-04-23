edd93d41dacef8bbebe0fc06c35f64d2cd6fded3
Anik Islam <anik54908@gmail.com>
2026-04-23 11:56:22 +0600

    initialize banner components

--- src/Components/OurService/OurService.jsx
+++ src/Components/OurService/OurService.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Service from '../../pages/Service'
+import CallToAction from '../../pages/CallToAction'
 
 const OurService = () => {
   return (
     <>
     <Service/>
+    <CallToAction/>
     </>
   )
 }
---
+++ src/pages/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '../Common/Container/Container'
+
+const Banner = () => {
+  return (
+    <>
+    <section>
+        <Container>
+            
+        </Container>
+    </section>
+    </>
+  )
+}
+
+export default Banner
