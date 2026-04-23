12e186cf118f6bb23101abf79cb3c71b5b8ac332
Anik Islam <anik54908@gmail.com>
2026-04-23 12:25:35 +0600

    created banner componenets and added it to home menu

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,10 +3,11 @@ import Services from "../../pages/Services";
 import About from "../../pages/About";
 import CallToAction from "../../pages/CallToAction";
 import Testimonial from "../../pages/Testimonial";
+import Banner from "../../pages/Banner";
 
 const Home = () => {
   return (
-    <>
+    <><Banner/>
       <Services />
       <About />
       <CallToAction />
--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import Container from '../Common/Container/Container'
+import BannerBg from '../../src/assets/img/banner.png'
 
 const Banner = () => {
   return (
     <>
-    <section>
-        <Container>
-            
-        </Container>
-    </section>
+      <section>
+        <div className="relative">
+          <img className="w-full h-auto" src={BannerBg} alt="banner bg" />
+
+          <div className="absolute inset-0 bg-black/50"></div>
+
+          <div className="absolute inset-0 flex items-center justify-center">
+            <h1 className="text-[#f16722] max-w-[850px] text-[76px] leading-25 font-semibold font-raleway text-center">
+              We are 10’s EVENT
+              <span className="text-white"> We bring your</span> dream event
+              <span className="text-white"> to life!</span>
+            </h1>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
 export default Banner
