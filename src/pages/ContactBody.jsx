c2567a83b6524e4df1882a6f0928b17295393172
Anik Islam <anik54908@gmail.com>
2026-04-30 14:47:49 +0600

    added side a map in contact us components

--- src/pages/ContactBody.jsx
+++ src/pages/ContactBody.jsx
@@ -1,81 +1,89 @@
 import React from "react";
 import Container from "../Common/Container/Container";
+import sideMap from "../../src/assets/img/contact-side-map.png";
 
 const ContactBody = () => {
   return (
     <>
       <section className="py-20">
         <Container>
-          <div className="  items-center gap-25">
-            {/* left part  */}
-            <div className="max-w-[641px]  ">
-              <h1 className="font-raleway text-[54px] text-[#000000] leading-17.5 font-semibold">
-                Say Hello!
-              </h1>
+          <div>
+            <div className="  items-center gap-25">
+              {/* left part  */}
+              <div className="max-w-[641px]  ">
+                <h1 className="font-raleway text-[54px] text-[#000000] leading-17.5 font-semibold">
+                  Say Hello!
+                </h1>
 
-              <form action="#" className="mt-8.75">
-                <div className="flex flex-col gap-5">
-                  {/* name  */}
-                  <div>
-                    <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
-                      Full Name
-                    </label>
-                    <br />
-                    <input
-                      className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
-                      type="text"
-                      placeholder="Enter Name"
-                    />
-                  </div>
+                <form action="#" className="mt-8.75">
+                  <div className="flex flex-col gap-5">
+                    {/* name  */}
+                    <div>
+                      <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
+                        Full Name
+                      </label>
+                      <br />
+                      <input
+                        className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
+                        type="text"
+                        placeholder="Enter Name"
+                      />
+                    </div>
 
-                  {/* number  */}
-                  <div>
-                    <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
-                      Conatact Number
-                    </label>
-                    <br />
-                    <input
-                      className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
-                      type="number"
-                      placeholder=" Conatact Number"
-                    />
-                  </div>
+                    {/* number  */}
+                    <div>
+                      <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
+                        Conatact Number
+                      </label>
+                      <br />
+                      <input
+                        className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
+                        type="number"
+                        placeholder=" Conatact Number"
+                      />
+                    </div>
 
-                  {/* email address  */}
-                  <div>
-                    <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
-                      Email Address
-                    </label>
-                    <br />
-                    <input
-                      className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
-                      type="email"
-                      placeholder="Email Address"
-                    />
-                  </div>
+                    {/* email address  */}
+                    <div>
+                      <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
+                        Email Address
+                      </label>
+                      <br />
+                      <input
+                        className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
+                        type="email"
+                        placeholder="Email Address"
+                      />
+                    </div>
 
-                  {/* message  */}
-                  <div>
-                    <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
-                      Message
-                    </label>
-                    <br />
-                    <textarea
-                      cols={5}
-                      rows={8}
-                      className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
-                      type="text"
-                      placeholder="Type your message here..."
-                    ></textarea>
+                    {/* message  */}
+                    <div>
+                      <label className="text-[18px] text-[#f16722] leading-8.75 font-semibold font-raleway mb-2">
+                        Message
+                      </label>
+                      <br />
+                      <textarea
+                        cols={5}
+                        rows={8}
+                        className="w-full text-[16px] font-raleway font-medium p-[12px_35px_12px_23px] border border-[#ACACAC] rounded-xl outline-none    "
+                        type="text"
+                        placeholder="Type your message here..."
+                      ></textarea>
+                    </div>
+                    <button className="bg-[#f16722] py-5 px-45 w-full rounded-md font-raleway font-semibold text-[18px] leading-8.75 text-white cursor-pointer">
+                      Send
+                    </button>
                   </div>
-                  <button className="bg-[#f16722] py-5 px-45 w-full rounded-md font-raleway font-semibold text-[18px] leading-8.75 text-white cursor-pointer">
-                    Send
-                  </button>
-                </div>
-              </form>
+                </form>
+              </div>
+              {/* right part  */}
+              <div></div>
             </div>
-            {/* right part  */}
-            <div></div>
+
+            {/* side map  */}
+            <div className="mt-20">
+              <img src={sideMap} alt="" />
+            </div>
           </div>
         </Container>
       </section>
