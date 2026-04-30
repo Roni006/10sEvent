58acc99e206b53defaea6372eba0dc60eb56b1a4
Anik Islam <anik54908@gmail.com>
2026-05-01 01:49:38 +0600

    I've initialized the footers' components

--- src/Layouts/RootLayOut.jsx
+++ src/Layouts/RootLayOut.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Common/Navbar/Navbar";
 import { Outlet } from "react-router";
+import Footer from "../pages/Footer";
 
 const RootLayOut = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Outlet />
+      <Footer/>
     </div>
   );
 };
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../Common/Container/Container'
+
+const Footer = () => {
+  return (
+    <>
+    <section className='pt-20'>
+        <Container>
+            <div className='bg-[#f16722]'>
+                <h1 className='text-[50px] font-bold text-white'>this is footer componets</h1>
+            </div>
+        </Container>
+    </section>
+    </>
+  )
+}
+
+export default Footer
